3692b4b7adb66cd40d88feb7bbf6c068f40f151d
Rudy <rudyzhou1234@gmail.com>
2018-07-27 17:20:46 -0400

    added license, cited multipleKymograph

---
+++ src/main/java/sc/fiji/multiKymograph/testPlugin_.java
@@ -0,0 +1,23 @@
+package sc.fiji.multiKymograph;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.WindowManager;
+import ij.gui.Line;
+import ij.gui.PolygonRoi;
+import ij.gui.Roi;
+import ij.plugin.PlugIn;
+import ij.process.FloatProcessor;
+import ij.process.ImageProcessor;
+
+import java.awt.Rectangle;
+
+public class testPlugin_ implements PlugIn{
+
+	@Override
+	public void run(String arg0) {
+		// TODO Auto-generated method stub
+		IJ.showMessage("Hi");
+	}
+
+}
